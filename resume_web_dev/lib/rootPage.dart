5dd752ba7348d8694501d7141d7e21d9a8b79d64
KKimj <kkimj@hanyang.ac.kr>
2020-08-05 13:21:07 +0900

    tab init

---
+++ resume_web_dev/lib/rootPage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:resumeweb/mainPage.dart';
+
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MainPage();
+  }
+}
+
+
